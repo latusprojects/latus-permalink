1261c417bcc6de7d496ab6f1cb90dc08087eb27f
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-09-01 17:35:49 +0200

    Add HasPermalinks.php trait

---
+++ src/Models/Traits/HasPermalinks.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Latus\Permalink\Models\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Latus\Permalink\Generators\Contracts\PermalinkGenerator;
+
+trait HasPermalinks
+{
+    public abstract function getPermalinkGenerator(): PermalinkGenerator;
+
+    public abstract function getPermalinkName(): string;
+
+    public abstract function getPermalinkDate(): string;
+
+    public function generatePermalink(string $syntax)
+    {
+        if (!($this instanceof Model)) {
+            throw new \TypeError('Target class "' . static::class . '" must be an instance of "Illuminate\Database\Eloquent\Model".');
+        }
+        
+        return $this->getPermalinkGenerator()->generate($this, $syntax);
+    }
+}
